81662ccbbca027ccc10dcd881ea0ad86f1ee6549
unknown <kaduhermanni@gmail.com>
2021-04-19 19:14:14 -0300

    add typing to crawlerservice and rename to crawlservice t crawlerservice

--- src/services/crawlService.ts
+++ src/services/crawlService.ts
@@ -2,4 +2,11 @@ import { axurApi } from "./axurClient"
 
 export const postCrawlTerm = (keyword: string) => axurApi.post('/crawl', { keyword: keyword }).then(res => res.data)
 
-export const getCrawlTerm = (keywordId: string) => axurApi.get(`/crawl/${keywordId}`).then(res => res.data)
+export const getCrawlTerm = (keywordId: string) => axurApi.get<IGetCrawlTerm>(`/crawl/${keywordId}`).then(res => res.data)
+
+interface IGetCrawlTerm {
+  id?: string,
+  status?: "active" | "done" | 400,
+  message?: string, 
+  urls?: string[]
+}
